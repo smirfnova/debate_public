aa007377241736bbcf2d0e0c7ca856567581537d
Shirley Cheng <ladyjane@Shirleys-MacBook-Pro.local>
2021-01-09 19:33:18 -0800

    correcting path

--- webServer.js
+++ webServer.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 //app.use(express.static(path.join(__dirname, 'build')));
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'src');
 app.use(express.static(publicPath));
 app.use(express.json())
 
