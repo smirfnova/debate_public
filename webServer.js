1cac7c14a056f8a996d9ba7504087f93445a3dbe
Shirley Cheng <ladyjane@Shirleys-MacBook-Pro.local>
2021-01-11 18:20:18 -0800

    procfile

--- webServer.js
+++ webServer.js
@@ -75,3 +75,10 @@ app.post('/query', function(req, res) {
 } querydb();
 });
 app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
